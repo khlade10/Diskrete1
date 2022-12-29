a1bd865edb30035d54bb2adf486d96a373f26d59
khlade10 <121464877+khlade10@users.noreply.github.com>
2022-12-29 23:18:32 +0200

    Add files via upload

---
+++ Program 1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read n1 and n2 from the user
+            Console.WriteLine("Enter n1: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter n2: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            // Print the prime numbers from n1 to n2
+            for (int i = n1; i <= n2; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static bool IsPrime(int n)
+        {
+            // Check if n is less than 2
+            if (n < 2)
+            {
+                return false;
+            }
+
+            // Check if n is a multiple of 2
+            if (n % 2 == 0)
+            {
+                return n == 2;
+            }
+
+            // Check if n is divisible by any odd number between 3 and the square root of n
+            for (int i = 3; i * i <= n; i += 2)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PerfectNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the lower and upper bounds of the range
+            Console.Write("Enter the lower bound of the range: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the upper bound of the range: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            // Find and print the perfect numbers in the range
+            Console.WriteLine("Perfect numbers in the range {0} to {1}:", n1, n2);
+            for (int i = n1; i <= n2; i++)
+            {
+                if (IsPerfect(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        // Returns true if the given number is a perfect number, false otherwise
+        static bool IsPerfect(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum == n;
+        }
+    }
+}
